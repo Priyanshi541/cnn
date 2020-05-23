5469d256fb7c5d843041aa40478d19e8dd53bf72
Priyanshi541 <64742449+Priyanshi541@users.noreply.github.com>
2020-05-24 01:33:22 +0530

    cnn code

--- Cat_Dog_Cnn.py
+++ Cat_Dog_Cnn.py
@@ -1,131 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 from keras.layers import Convolution2D
-
-
-# In[2]:
-
 
 from keras.layers import MaxPooling2D
 
-
-# In[3]:
-
-
 from keras.layers import Flatten
-
-
-# In[4]:
-
 
 from keras.layers import Dense
 
-
-# In[5]:
-
-
 from keras.models import Sequential
-
-
-# In[6]:
-
 
 model = Sequential()
 
-
-# In[7]:
-
-
 model.add(Convolution2D (filters=32, kernel_size=(3,3), activation='relu',input_shape=(64, 64, 3)) )
-
-
-# In[8]:
-
 
 model.summary()
 
-
-# In[9]:
-
-
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
-
-# In[10]:
-
 
 model.add(Convolution2D(filters=32, 
                         kernel_size=(3,3), 
                         activation='relu',
                        ))
 
-
-# In[11]:
-
-
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-
-# In[12]:
-
-
 model.summary()
-
-
-# In[13]:
-
 
 model.add(Flatten())
 
-
-# In[14]:
-
-
 model.summary()
-
-
-# In[15]:
-
 
 model.add(Dense(units=128, activation='relu'))
 
-
-# In[16]:
-
-
 model.summary()
-
-
-# In[17]:
-
 
 model.add(Dense(units=1, activation='sigmoid'))
 
-
-# In[18]:
-
-
 model.summary()
-
-
-# In[19]:
-
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-
-# In[20]:
-
-
 from keras_preprocessing.image import ImageDataGenerator
-
-
-# In[21]:
-
 
 train_datagen = ImageDataGenerator(
         rescale=1./255,
@@ -150,127 +67,4 @@ model.fit(
         validation_data=test_set,
         validation_steps=800)
 
-
-# In[ ]:
-
-
-# model.save('my.h5')
-
-
-# In[ ]:
-
-
-from keras.models import load_model
-
-
-# In[ ]:
-
-
-m = load_model('cnn-cat-dog-model.h5')
-
-
-# In[ ]:
-
-
-from keras.preprocessing import image
-
-
-# In[ ]:
-
-
-test_image = image.load_img('cnn_dataset/single_prediction/cat_or_dog_2.jpg', 
-               target_size=(64,64))
-
-
-# In[ ]:
-
-
-type(test_image)
-
-
-# In[ ]:
-
-
-test_image
-
-
-# In[ ]:
-
-
-test_image = image.img_to_array(test_image)
-
-
-# In[ ]:
-
-
-type(test_image)
-
-
-# In[ ]:
-
-
-test_image.shape
-
-
-# In[ ]:
-
-
-import numpy as np 
-
-
-# In[ ]:
-
-
-test_image = np.expand_dims(test_image, axis=0)
-
-
-# In[ ]:
-
-
-test_image.shape
-
-
-# In[ ]:
-
-
-result = m.predict(test_image)
-
-
-# In[ ]:
-
-
-result
-
-
-# In[ ]:
-
-
-if result[0][0] == 1.0:
-    print('dog')
-else:
-    print('cat')
-
-
-# In[ ]:
-
-
-r = training_set.class_indices
-
-
-# In[ ]:
-
-
-r
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+model.save('my.h5')
