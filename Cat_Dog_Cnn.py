1df015cef0b83e491057107be8007069dea6bc71
Priyanshi541 <64742449+Priyanshi541@users.noreply.github.com>
2020-05-25 23:18:21 +0530

    Update Cat_Dog_Cnn.py

--- Cat_Dog_Cnn.py
+++ Cat_Dog_Cnn.py
@@ -12,6 +12,8 @@ model = Sequential()
 
 neurons1 = 512
 
+neurons2 = 256
+
 epochs1 = 5
 
 model.add(Convolution2D (filters = 32, kernel_size=(3,3), activation='relu',input_shape=(64, 64, 3)) )
@@ -26,6 +28,8 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 
 model.add(Dense(units= neurons1 , activation='relu'))
+
+model.add(Dense(units= neurons2 , activation='relu'))
 
 model.add(Dense(units=1, activation='sigmoid'))
 
